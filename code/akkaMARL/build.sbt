c09b6f5e2776b38f809d03a0c337176dd1e4909d
Matthias Humt <matthias.humt@mailbox.org>
2017-12-29 11:37:26 +0100

    MARL with akka skeleton

---
+++ code/akkaMARL/build.sbt
@@ -0,0 +1,5 @@
+name := "sbtExampleProject"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
---
+++ code/akkaMARL/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Hello {
+  val who = "Activator"
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, " + who + "!")
+    println("Goodbye, " + who + "!")
+  }
+}
