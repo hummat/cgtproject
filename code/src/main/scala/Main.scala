e6f9f80c295f6a4b98f12bee727e59846287019b
sthomer <steven.t.homer@gmail.com>
2018-01-07 19:31:40 +0100

    324, 729 baseline and 9sup scaled

--- code/src/main/scala/Main.scala
+++ code/src/main/scala/Main.scala
@@ -16,17 +16,17 @@ import java.io._
 object Main extends App {
 
   // Experiment Parameters
-  val filename = "N_one_sup_w25n325.csv"
+  val filename = "N_9sup_n729_scaled.csv"
   val maxSteps = 10000
-  val numSupervisors = 1
-  val numSubordinates = 325
+  val numSupervisors = 9
+  val numSubordinates = 729
   val window = 25
   val trials = 10
   val noise = 0.0 // wtf is juice???
 
   // Other Parameters
-  val maxBranchingFactor = 10
-  val maxServiceTime = 50
+  val maxBranchingFactor = 70
+  val maxServiceTime = 350
 
   val bw = new BufferedWriter(new FileWriter(new File(filename)))
   bw.write("trial,step,original,complete,window,sups,size\n")
